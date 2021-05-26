9171da2a0f68fb240894672bd432990eb34960b3
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-05-26 15:19:18 -0500

    Change option atom to string

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@ import Config
 
 config :phx_bb, PhxBbWeb.Endpoint,
   server: true,
-  url: [scheme: :https, host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [scheme: "https", host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
 
 config :phx_bb, PhxBb.Mailer,
   adapter: Swoosh.Adapters.Mailjet,
