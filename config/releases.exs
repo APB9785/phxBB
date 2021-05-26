0660108a22237375edb77b25bc888779c677f3db
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-05-26 14:39:44 -0500

    Update releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,6 @@ import Config
 
 config :phx_bb, PhxBbWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
 
 config :phx_bb, PhxBb.Mailer,
