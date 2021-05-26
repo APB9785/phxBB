194a17ae9964ec9ecf18e309a6d7a1ecf3e51906
APB9785 <74077809+APB9785@users.noreply.github.com>
2021-05-26 13:43:10 -0500

    Add Mailjet config to releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -4,3 +4,8 @@ config :phx_bb, PhxBbWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+
+config :phx_bb, PhxBb.Mailer,
+  adapter: Swoosh.Adapters.Mailjet,
+  api_key: System.get_env("MAILJET_API_KEY"),
+  secret: System.get_env("MAILJET_SECRET_KEY")
