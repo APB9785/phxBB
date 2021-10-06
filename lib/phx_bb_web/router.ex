acb83eaaa47314ad7854f260ddf1f26107f729c2
Andrew P Berrien <74077809+APB9785@users.noreply.github.com>
2021-10-06 18:12:44 -0500

    Add content-security-policy header

--- lib/phx_bb_web/router.ex
+++ lib/phx_bb_web/router.ex
@@ -9,7 +9,7 @@ defmodule PhxBbWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {PhxBbWeb.LayoutView, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
     plug :fetch_current_user
   end
 
