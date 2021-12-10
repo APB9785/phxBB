6b8baab877682929ee7964d971f48f84cd02aa09
Andrew Berrien <74077809+APB9785@users.noreply.github.com>
2021-12-10 15:10:17 -0600

    Bump esbuild

--- config/config.exs
+++ config/config.exs
@@ -32,7 +32,7 @@ config :phx_bb, PhxBb.Mailer,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
+  version: "0.14.0",
   default: [
     args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
