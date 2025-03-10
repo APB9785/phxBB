d98318e36bc5956a962e80d11a132b9c109e5e51
APB9785 <74077809+APB9785@users.noreply.github.com>
2025-03-10 15:58:24 -0500

    user menu

--- lib/phx_bb_web/components/app_components.ex
+++ lib/phx_bb_web/components/app_components.ex
@@ -1,11 +1,12 @@
 defmodule PhxBbWeb.AppComponents do
 
+  attr :id, :string
   attr :current_user, :map
   attr :post, :map
 
   def author_infobox(assigns) do
     ~H"""
-    <div class={post_author_style(@current_user)}>
+    <div class={post_author_style(@current_user)} id={@id}>
   <div>
     <.link patch={~p"/forum/user/#{@post.author.id}"}
       class={StyleHelpers.link_style(@current_user)}
@@ -53,4 +54,66 @@ defmodule PhxBbWeb.AppComponents do
   defp author_dividers(nil), do: author_dividers(%User{theme: StyleHelpers.default()})
   defp author_dividers(%User{theme: "elixir"}), do: "border-b"
   defp author_dividers(%User{theme: "dark"}), do: "border-b border-gray-500"
+
+  attr :current_user, :map
+  attr :unread_messages, :list
+
+  def user_menu(assigns) do
+    ~H"""
+    <%= case @current_user do %>
+  <% nil -> %>
+    <div class={user_menu(nil)} id="logged-out-menu">
+      <div class="flex justify-center">
+        <div>
+          <.link patch={~p"/register"} id="user-menu-register" class={StyleHelpers.link_style(nil)}>Register</.link>
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <div>
+          <.link patch={~p"/login"} id="user-menu-login" class={StyleHelpers.link_style(nil)}>Log in</.link>
+        </div>
+      </div>
+    </div>
+  <% user -> %>
+    <div class={user_menu(user)} id="logged-in-menu">
+      <div class="text-center">
+        Logged in as <.link patch={~p"/users/#{user.id}"} id="user-menu-profile" class={StyleHelpers.link_style(user)}>{user.username}</.link>
+        <br />
+      </div>
+      <div class="flex justify-center flex-wrap">
+        <%= if user.admin do %>
+          <div>
+            <.link patch={~p"/admin"} id="user-menu-admin" class={StyleHelpers.link_style(user)}>Admin Panel</.link>
+          </div>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        <% end %>
+        <div>
+          <.link patch={~p"/settings"} id="user-menu-settings" class={StyleHelpers.link_style(user)}>Settings</.link>
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <div>
+          <.link patch={~p"/logout"} id="user-menu-logout" class={StyleHelpers.link_style(user)}>Log out</.link>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <div>
+          <.link patch={~p"/messages"} id="user-menu-messages" class={StyleHelpers.link_style(user)}>Messages</.link>
+        </div>
+        <%= if @unread_messages > 0 do %>
+          <div id="unread-message-notification">
+            &nbsp;({@unread_messages})
+          </div>
+        <% end %>
+      </div>
+    </div>
+<% end %>
+    """
+  end
+
+  defp user_menu(user) do
+    [
+      "py-2 max-w-sm mx-auto rounded-lg md:rounded-md shadow-md antialiased ",
+      "relative opacity-100 font-sans ",
+      StyleHelpers.content_bg_theme(user)
+    ]
+  end
 end
